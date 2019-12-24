a91b2c676817ce07fe32603a9b4e0a6bd9c9fa1f
Iurii Solntsev <iurii_solntsev@epam.com>
2019-12-24 18:49:49 +0300

    VS wizard generated C++ COM ATL DLL frame

---
+++ Drill4dotNet/Drill4dotNet/dllmain.cpp
@@ -0,0 +1,16 @@
+// dllmain.cpp : Implementation of DllMain.
+
+#include "pch.h"
+#include "framework.h"
+#include "resource.h"
+#include "Drill4dotNet_i.h"
+#include "dllmain.h"
+
+CDrill4dotNetModule _AtlModule;
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved);
+}
