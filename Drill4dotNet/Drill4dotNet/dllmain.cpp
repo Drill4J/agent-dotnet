1ec023ca3437256b9ad54cda13788485788ac0b0
Iurii Solntsev <iurii_solntsev@epam.com>
2019-12-25 13:19:14 +0300

    Tabs to spaces

--- Drill4dotNet/Drill4dotNet/dllmain.cpp
+++ Drill4dotNet/Drill4dotNet/dllmain.cpp
@@ -11,6 +11,6 @@ CDrill4dotNetModule _AtlModule;
 // DLL Entry Point
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
-	hInstance;
-	return _AtlModule.DllMain(dwReason, lpReserved);
+    hInstance;
+    return _AtlModule.DllMain(dwReason, lpReserved);
 }
--- Drill4dotNet/Drill4dotNet/dllmain.h
+++ Drill4dotNet/Drill4dotNet/dllmain.h
@@ -2,9 +2,9 @@
 
 class CDrill4dotNetModule : public ATL::CAtlDllModuleT< CDrill4dotNetModule >
 {
-public :
-	DECLARE_LIBID(LIBID_Drill4dotNetLib)
-	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_DRILL4DOTNET, "{bb54e37e-3e2e-4b45-ad99-223cb351098e}")
+public:
+    DECLARE_LIBID(LIBID_Drill4dotNetLib)
+    DECLARE_REGISTRY_APPID_RESOURCEID(IDR_DRILL4DOTNET, "{bb54e37e-3e2e-4b45-ad99-223cb351098e}")
 };
 
 extern class CDrill4dotNetModule _AtlModule;
