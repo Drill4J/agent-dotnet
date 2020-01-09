f16d81fcfc729103e8d8008f76e1549f04647692
Iurii Solntsev <iurii_solntsev@epam.com>
2020-01-09 15:12:19 +0300

    Refined runtime environment settings.

--- setruntimeenv.cmd
+++ setruntimeenv.cmd
@@ -1,7 +1,10 @@
 @echo Setting .NET Core run-time configuration options for profiling...
 set COMPlus_EnableDiagnostics=1
 set CORECLR_ENABLE_PROFILING=1
-set CORECLR_PROFILER=BB54E37E-3E2E-4B45-AD99-223CB351098E
+rem Define Profiler either by CLSID:
+set CORECLR_PROFILER={BE4D1E40-1A9F-45E4-BCE4-68DE191CC8D6}
+rem ...or by ProgId name:
+set CORECLR_PROFILER=Drill4dotNet.DrillProfiler
 set CORECLR_PROFILER_PATH=.\Drill4dotNet\bin\x64\Release\Drill4dotNet.dll
 @echo Also setting .NET Framework run-time configuration options...
 set COR_ENABLE_PROFILING=%CORECLR_ENABLE_PROFILING%
