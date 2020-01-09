fd90c8995d816428df79e166fd1b2903cd8d9eb5
Iurii Solntsev <iurii_solntsev@epam.com>
2020-01-09 15:57:43 +0300

    Log enter to `DllMain()` and `DisableThreadLibraryCalls()` to suppress flood.

--- Drill4dotNet/Drill4dotNet/dllmain.cpp
+++ Drill4dotNet/Drill4dotNet/dllmain.cpp
@@ -5,12 +5,17 @@
 #include "resource.h"
 #include "Drill4dotNet_i.h"
 #include "dllmain.h"
+#include <iostream>
 
 CDrill4dotNetModule _AtlModule;
 
 // DLL Entry Point
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
-    hInstance;
+    if (DLL_PROCESS_ATTACH == dwReason)
+    {
+        DisableThreadLibraryCalls(hInstance);
+    }
+    std::wcout << L"DllMain(" << hInstance << L", " << dwReason << L")\n";
     return _AtlModule.DllMain(dwReason, lpReserved);
 }
