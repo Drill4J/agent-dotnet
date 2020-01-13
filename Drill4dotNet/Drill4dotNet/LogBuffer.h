35563a07cd17d4d471fa09b55b511e46f653c870
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-01-13 16:57:45 +0300

    Forbid to store LogBuffer outside of expressions. Also, add documentation.

--- Drill4dotNet/Drill4dotNet/LogBuffer.h
+++ Drill4dotNet/Drill4dotNet/LogBuffer.h
@@ -5,13 +5,34 @@
 
 namespace Drill4dotNet
 {
+    // Wrapper for another stream. Adds a line break each time
+    // when a set of << operators is over. All instances are
+    // temporary and forbidden to be stored in variables.
+    // Example:
+    // auto f() { return LogBuffer(std::cout); }
+    //
+    // void main()
+    // {
+    //     f() << "Hello, " << "world";
+    //     // Line break inserted to the output;
+    //     f() << 1 << 2 << 3;
+    //     // Another line break inserted to the output;
+    // }
     template <typename TStream>
     class LogBuffer
     {
     private:
+        // It is safe to have a reference here.
+        // This class is neither copyable not moveable.
+        // An instance cannot live longer than the expression,
+        // which created it, is being executed - because
+        // temporary objects are not deleted in the middle of
+        // an expression.
         TStream& m_output;
         std::wostringstream m_buffer{};
     public:
+        // Creates a new instance.
+        // output : the target stream.
         explicit LogBuffer(TStream& output)
             : m_output(output)
         {
@@ -22,8 +43,11 @@ namespace Drill4dotNet
             m_output << m_buffer.str() << std::endl;
         }
 
-        LogBuffer(LogBuffer&&) = default;
-        LogBuffer& operator=(LogBuffer&&) = default;
+        // This class should not be neither copyable nor moveable.
+        // Otherwise, it is possible to keer the instance after
+        // the expression, which created it, is finished.
+        // In this case it is possible that the m_output reference
+        // to become stale.
         LogBuffer(const LogBuffer&) = delete;
         LogBuffer& operator=(const LogBuffer&) = delete;
 
@@ -35,6 +59,8 @@ namespace Drill4dotNet
         }
     };
 
+    // Makes an output tool, which automatically adds
+    // a line break at the end of series of << operators.
     inline LogBuffer<std::wostream> LogStdout()
     {
         return LogBuffer<std::wostream>(std::wcout);
