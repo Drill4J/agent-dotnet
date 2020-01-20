a08feb4fa1333d4b2b8c8a39c24a9cb3ef81683b
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-01-20 14:17:31 +0300

    Make united solution file to allow debugging the profiler.
    Previously, there were 2 different solutions. It required to run the test app from the command line, making it difficult to debug the attached profiler.
    Also, user has to manually run the setup cript under admin.
    
    Now, there is only 1 solution, and the structure of output folders was changed.
    With these changes, the profiler dll is not registered in the registry anymore, and admin access therefore not required.
    Also, set of the environment variables is refined.
    For the .NET Core's Hello World application, the variables are set in the launch json configuration.
    User can start the project from Visual Studio, and he will get the profiler attached and will be able to debug it.
    For the .NET Framework's HelloWorldFramework application, it looks difficult to set up environment variables to run from Visual Studio. But the required set of variables is saved to RunScript.cmd.
    User can double-click the script in the project output directory, and he will get the application running with debugger.
    
    Additionally, setruntimeenv.cmd has been updated to reflect the removal of COM registration and intermediate directory for C++ project has been renamed to obj.

--- setruntimeenv.cmd
+++ setruntimeenv.cmd
@@ -3,14 +3,10 @@ set COMPlus_EnableDiagnostics=1
 set CORECLR_ENABLE_PROFILING=1
 rem Define Profiler either by CLSID:
 set CORECLR_PROFILER={BE4D1E40-1A9F-45E4-BCE4-68DE191CC8D6}
-rem ...or by ProgId name:
-set CORECLR_PROFILER=Drill4dotNet.DrillProfiler
-set CORECLR_PROFILER_PATH=.\Drill4dotNet\bin\x64\Release\Drill4dotNet.dll
+set CORECLR_PROFILER_PATH=%~dp0.\bin\x64\Release\Drill4dotNet\Drill4dotNet.dll
 @echo Also setting .NET Framework run-time configuration options...
 set COR_ENABLE_PROFILING=%CORECLR_ENABLE_PROFILING%
 set COR_PROFILER=%CORECLR_PROFILER%
 set COR_PROFILER_PATH=%CORECLR_PROFILER_PATH%
-@echo Registering COM component...
-regsvr32 -s "%CORECLR_PROFILER_PATH%" || echo Registration failed.
 @echo Setting compatibility for .NET Framework 4.x due to use outdated profiler...
 set COMPlus_ProfAPI_ProfilerCompatibilitySetting=EnableV2Profiler
