dcfc89487dc27183d636fe8ab0506e33d0e1e85b
Iurii Solntsev <iurii_solntsev@epam.com>
2020-01-25 21:51:45 +0300

    Housekeeping: Include often used STL headers to precompiled headers

--- Drill4dotNet/Drill4dotNet/pch.h
+++ Drill4dotNet/Drill4dotNet/pch.h
@@ -8,3 +8,11 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
+
+// Often used STL headers
+#include <cstddef>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <vector>
