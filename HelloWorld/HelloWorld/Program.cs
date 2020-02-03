b3ffac6fba49afd4a9e98450aae15f708c546d9a
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-02-03 10:54:05 +0300

    EPMDJ-2224: Example injection.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,9 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            MyInjectionTarget();
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Press Enter.");
             Console.ReadLine();
+        }
+
+        private static void MyInjectionTarget()
+        {
+            int x = 1;
+            int y = 2;
+            int z = x + y;
+            Console.WriteLine(z);
         }
     }
 }
