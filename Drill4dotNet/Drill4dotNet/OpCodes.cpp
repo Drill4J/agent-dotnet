cd99a20d29788fa684c2cd7efadf621e0b71b349
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-02-03 10:52:14 +0300

    EPMDJ-2224: Introduce OpCodeVariant class to hold one of possible instructions.

--- Drill4dotNet/Drill4dotNet/OpCodes.cpp
+++ Drill4dotNet/Drill4dotNet/OpCodes.cpp
@@ -3,5 +3,59 @@
 
 namespace Drill4dotNet
 {
+    OpCodeVariant::OpCodeVariant(
+        InstructionCode code,
+        VariantType argument)
+        : m_code(code),
+        m_argument { argument }
+    {
+    }
+
+    OpCodeVariant::OpCodeVariant()
+        : m_code(OpCodeInstruction<OpCode_CEE_NOP>::Code),
+        m_argument {}
+    {
+    }
+
+    bool OpCodeVariant::HasArgument() const
+    {
+        return !std::holds_alternative<std::monostate>(m_argument);
+    }
+
+    uint32_t OpCodeVariant::SizeWithArgument() const
+    {
+        uint32_t instructionSize { m_code.Size() };
+        return instructionSize + std::visit([](const auto& argument)
+            {
+                using T = std::decay_t<decltype(argument)>;
+                if constexpr (std::is_same_v<T, std::monostate>)
+                {
+                    return static_cast<uint32_t>(0);
+                }
+                else if constexpr (std::is_same_v<T, OpCodeArgumentType::InlinePhi>)
+                {
+                    return static_cast<uint32_t>(argument.size() * sizeof(uint16_t) + sizeof(uint8_t));
+                }
+                else if constexpr (std::is_same_v<T, OpCodeArgumentType::InlineSwitch>)
+                {
+                    return static_cast<uint32_t>(argument.size() * sizeof(int32_t) + sizeof(int32_t));
+                }
+                else
+                {
+                    return static_cast<uint32_t>(sizeof(argument));
+                }
+            },
+            m_argument);
+    }
+
+    void OpCodeVariant::InstructionCode::AppendToVector(std::vector<std::byte>& target) const
+    {
+        if (!IsOneByte())
+        {
+            target.push_back(FirstByte);
+        }
+
+        target.push_back(SecondByte);
+    }
 }
 
--- Drill4dotNet/Drill4dotNet/OpCodes.h
+++ Drill4dotNet/Drill4dotNet/OpCodes.h
@@ -3,6 +3,9 @@
 #include <cstdint>
 #include <vector>
 #include <string_view>
+#include <optional>
+#include <variant>
+#include <type_traits>
 
 namespace Drill4dotNet
 {
@@ -166,5 +169,294 @@ namespace Drill4dotNet
 #include <opcode.def>
 #include "UnDefineOpCodesGeneratorSpecializations.h"
 #undef OPDEF_REAL_INSTRUCTION
+
+    // Provides compile-time operations on a set of types.
+    template <typename ...>
+    class ParameterPack;
+
+    // Allows to merge two sets of types.
+    template <typename TPack1, typename TPack2>
+    class Tail;
+
+    // Merging two sets of types: primary specialization.
+    template <typename ... TFirst, typename ... TSecond>
+    class Tail<ParameterPack<TFirst ...>, ParameterPack<TSecond ...>>
+    {
+    public:
+        using Type = typename ParameterPack<TFirst ..., TSecond ...>;
+    };
+
+    // Compile-time operations for 1 type.
+    template <typename T>
+    class ParameterPack<T>
+    {
+    public:
+        // Applies the type to the given template.
+        template <template <typename T> class TTemplate>
+        using Apply = TTemplate<T>;
+
+        // Provides a set of types with removed duplicates.
+        using RemoveDuplicates = typename ParameterPack<T>;
+    };
+
+    // Compile-time operations for 2 or more types.
+    template <typename T, typename T2, typename ... Ts>
+    class ParameterPack<T, T2, Ts...>
+    {
+    public:
+        // Applies the given types to given template.
+        template <template <typename T, typename T2, typename ... Ts> class TTemplate>
+        using Apply = TTemplate<T, T2, Ts ...>;
+
+        // Provides a set of types with duplicates removed.
+        using RemoveDuplicates = typename std::conditional_t <
+            std::is_same_v<T, T2> || ( std::is_same_v<T, Ts> || ... ),
+            typename ParameterPack<T2, Ts ...>::RemoveDuplicates,
+            typename Tail<ParameterPack<T>, typename ParameterPack<T2, Ts ...>::RemoveDuplicates>::Type>;
+    };
+
+    // Stores one of possible Intermediate Language instructions.
+    class OpCodeVariant
+    {
+    private:
+        // Variant which can store any possible inline argument type,
+        // std::monostate is for no inline argument case.
+        using VariantType = ParameterPack<
+            std::monostate,
+            OpCodeArgumentType::InlineBrTarget,
+            OpCodeArgumentType::InlineField,
+            OpCodeArgumentType::InlineI,
+            OpCodeArgumentType::InlineI8,
+            OpCodeArgumentType::InlineMethod,
+            OpCodeArgumentType::InlinePhi,
+            OpCodeArgumentType::InlineR,
+            OpCodeArgumentType::InlineRVA,
+            OpCodeArgumentType::InlineSig,
+            OpCodeArgumentType::InlineString,
+            OpCodeArgumentType::InlineSwitch,
+            OpCodeArgumentType::InlineTok,
+            OpCodeArgumentType::InlineType,
+            OpCodeArgumentType::InlineVar,
+            OpCodeArgumentType::ShortInlineBrTarget,
+            OpCodeArgumentType::ShortInlineI,
+            OpCodeArgumentType::ShortInlineR,
+            OpCodeArgumentType::ShortInlineVar
+        >::RemoveDuplicates::Apply<std::variant>;
+
+        // Represents instruction code. Has maximum 2 bytes,
+        // but some codes can be in 1-byte form.
+        class InstructionCode
+        {
+        private:
+            inline static const std::byte s_OneByteMarker = std::byte(0xFF);
+
+        public:
+            // The first byte. Can be a one-byte form marker.
+            std::byte FirstByte;
+
+            // The second byte.
+            std::byte SecondByte;
+
+            // Gets the value indicating whether this code
+            // can be in 1-byte form.
+            constexpr bool IsOneByte() const
+            {
+                return FirstByte == s_OneByteMarker;
+            }
+
+            // Gets the size of binary representation, in bytes.
+            constexpr uint32_t Size() const
+            {
+                return IsOneByte() ? 1 : 2;
+            }
+
+            // Gets the combined uint16_t value to use in switch-case statements.
+            constexpr uint16_t AsKey() const
+            {
+                return static_cast<uint16_t>(FirstByte) << 8 | static_cast<uint16_t>(SecondByte);
+            }
+
+            // Append the binary representation to the given vector.
+            // @param target : the vector to append to.
+            void AppendToVector(std::vector<std::byte>& target) const;
+
+            // Compares two codes for equality.
+            constexpr bool operator==(const InstructionCode other) const
+            {
+                return FirstByte == other.FirstByte && SecondByte == other.SecondByte;
+            }
+
+            // Compares two codes for inequality.
+            constexpr bool operator!=(const InstructionCode other) const
+            {
+                return !(*this == other);
+            }
+        };
+
+        // Allows to get an instruction code for a
+        // specific OpCode type.
+        // The next call to <opcode.def> will specialize
+        // for each possible instruction.
+        template <typename TOpCode>
+        struct OpCodeInstruction;
+
+#define OPDEF_REAL_INSTRUCTION( \
+    canonicalName, \
+    stringName, \
+    stackPop, \
+    stackPush, \
+    inlineArgumentType, \
+    operationKind, \
+    codeLength, \
+    byte1, \
+    byte2, \
+    controlBehavior) \
+template <> \
+class OpCodeInstruction<OpCode_ ## canonicalName> \
+{ \
+public: \
+    static constexpr InstructionCode Code { std::byte { byte1 }, std::byte { byte2 } }; \
+};
+
+#include "DefineOpCodesGeneratorSpecializations.h"
+#include <opcode.def>
+#include "UnDefineOpCodesGeneratorSpecializations.h"
+#undef OPDEF_REAL_INSTRUCTION
+
+        // Stores the instruction code.
+        InstructionCode m_code;
+
+        // Stores the inline argument.
+        VariantType m_argument;
+
+        // Intended to use from MethodBody.
+        OpCodeVariant(
+            InstructionCode code,
+            VariantType argument);
+
+        // Gets the opcode of the given type,
+        // with the argument stored in this variant.
+        // Does not do any checks, so made private.
+        template <typename TOpCode>
+        TOpCode Get() const
+        {
+            if constexpr (TOpCode::HasArgument())
+            {
+                return TOpCode(std::get<TOpCode::ArgumentType>(m_argument));
+            }
+            else
+            {
+                return TOpCode{};
+            }
+        }
+
+    public:
+        // Constructs a variant holding OpCode_CEE_NOP instruction,
+        // which means no operation.
+        OpCodeVariant();
+
+        // Value indicating whether the given class is a valid OpCode class.
+        template <typename TOpCode>
+        static constexpr bool IsOpCodeClass = std::is_assignable_v<OpCodeTag&, TOpCode>;
+
+        // Constructs the variant holding the given opcode.
+        // @param opCode : instruction to store.
+        template <typename TOpCode, std::enable_if_t<IsOpCodeClass<TOpCode>, int> = 0>
+        OpCodeVariant(const TOpCode opCode)
+            : m_code (OpCodeInstruction<TOpCode>::Code),
+            m_argument{}
+        {
+            if constexpr (TOpCode::HasArgument())
+            {
+                m_argument = opCode.Argument();
+            }
+        }
+
+        // Puts the given opcode into this variant.
+        // @param opCode : the instruction to store.
+        template <typename TOpCode, std::enable_if_t<IsOpCodeClass<TOpCode>, int> = 0>
+        OpCodeVariant& operator=(const TOpCode opCode)
+        {
+            m_code = OpCodeInstruction<TOpCode>::Code;
+            if constexpr (TOpCode::HasArgument())
+            {
+                m_argument = opCode.Argument();
+            }
+            else
+            {
+                m_argument = std::monostate{};
+            }
+
+            return *this;
+        }
+
+        // Gets the value indicating whether the
+        // variant contains an opcode of the given type.
+        // TOpCode : the opcode type to check for.
+        template <typename TOpCode, std::enable_if_t<IsOpCodeClass<TOpCode>, int> = 0>
+        bool HoldsAlternative() const
+        {
+            return m_code == OpCodeInstruction<TOpCode>::Code;
+        }
+
+        // If the variant contains an opcode of the given type,
+        // gets it. std::nullopt otherwise.
+        // TOpCode : the opcode type to check for.
+        template <typename TOpCode, std::enable_if_t<IsOpCodeClass<TOpCode>, int> = 0>
+        std::optional<TOpCode> GetIf() const
+        {
+            if (HoldsAlternative<TOpCode>())
+            {
+                return Get<TOpCode>();
+            }
+            else
+            {
+                return std::nullopt;
+            }
+        }
+
+        // Gets the value indicating whether the opcode
+        // this variant contains has an inline argument.
+        bool HasArgument() const;
+
+        // Gets the size of binary representation of the
+        // opcode this variant contains, including the instruction
+        // code and the inline argument (if any).
+        uint32_t SizeWithArgument() const;
+
+        // Calls the visitor with the instruction this variant contains.
+        // TVisitor must provide overload for each possible OpCode_* class.
+        // This is usually done with a template lambda.
+        // @param : must have method accepting the OpCode_* instance.
+        template <typename TVisitor>
+        void Visit(TVisitor&& visitor) const
+        {
+            switch (m_code.AsKey())
+            {
+#define OPDEF_REAL_INSTRUCTION( \
+    canonicalName, \
+    stringName, \
+    stackPop, \
+    stackPush, \
+    inlineArgumentType, \
+    operationKind, \
+    codeLength, \
+    byte1, \
+    byte2, \
+    controlBehavior) \
+            case InstructionCode { std::byte { byte1 }, std::byte { byte2 } }.AsKey() : \
+            { \
+                visitor(Get< OpCode_ ## canonicalName >()); \
+            } \
+\
+            break;
+
+#include "DefineOpCodesGeneratorSpecializations.h"
+#include <opcode.def>
+#include "UnDefineOpCodesGeneratorSpecializations.h"
+#undef OPDEF_REAL_INSTRUCTION
+            }
+        }
+    };
 }
 
