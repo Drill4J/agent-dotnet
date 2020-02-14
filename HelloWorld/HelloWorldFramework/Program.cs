875d3f65b9cab87f79e1036e947d150b2aa5c58f
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-02-14 16:17:54 +0300

    EPMDJ-2315: Improve MyInjectionTarget methods, to demo the branching and exception handling support
    Additionally, debugger break has been replaced with calls to Console.WriteLine, inserted by the profiler.

--- HelloWorld/HelloWorldFramework/Program.cs
+++ HelloWorld/HelloWorldFramework/Program.cs
@@ -19,8 +19,35 @@ namespace HelloWorld
         {
             int x = 1;
             int y = 2;
-            int z = x + y;
-            Console.WriteLine(z);
+            int z;
+            try
+            {
+                z = x + y;
+                Console.WriteLine("Greetings from MyInjectionTarget");
+            }
+            catch (Exception)
+            {
+                z = 0;
+            }
+            finally
+            {
+                string s = "Greetings from the Profiler";
+                // For Debug builds, will inject
+                // Console.WriteLine(s);
+                // Such injection is not possible with Release builds,
+                // because the C# compiler optimizes s variable away.
+            }
+
+            if (z % 2 == 1)
+            {
+                Console.WriteLine(z);
+                // Will inject here
+                // if (x > 0)
+                // {
+                //     Console.WriteLine(42);
+                // }
+                // and 128 NOP operations
+            }
         }
     }
 }
