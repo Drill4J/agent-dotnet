870ff1128415d69e34de134034d8d489718e5345
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-02-27 14:02:50 +0300

    Intermediate: stub for CodeSize.

--- Drill4dotNet/Drill4dotNet/CodeSize.h
+++ Drill4dotNet/Drill4dotNet/CodeSize.h
@@ -1,5 +1,45 @@
 #pragma once
 
+#include <cstdint>
+#include "ByteUtils.h"
+
 namespace Drill4dotNet
 {
+    // Unit to measure, in bytes, sizes of instructions,
+    // sizes of instructions streams.
+    class CodeSize
+    {
+    private:
+        uint32_t m_size;
+
+    public:
+        using NonOverflowingSize = int64_t;
+
+        constexpr static bool IsValidCodeSize(NonOverflowingSize size) noexcept
+        {
+            // return !Overflows...
+        }
+
+        constexpr CodeSize(uint32_t size) noexcept
+        {}
+
+        constexpr CodeSize(NonOverflowingSize size)
+        {
+            if (!IsValidCodeSize(size))
+            {
+                throw 0; // ...
+            }
+        }
+
+        operator uint32_t() const noexcept;
+        operator NonOverflowing() const noexcept;
+
+        operator <<() const;
+
+    
+    };
+
+    NonOverflowing operator +(CodeSize a, CodeSize b);
+    NonOverflowing operator +(CodeSize::NonOverflowing a, CodeSize b);
+    NonOverflowing operator +(CodeSize a, CodeSize::NonOverflowing b);
 }
