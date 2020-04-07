4f42e3fdd7ce40c8be2df6bcec8dc44b88c4e14d
Aleksandr Kapustin <Aleksandr_Kapustin@epam.com>
2020-04-07 10:31:13 +0300

    EPMDJ-2091/EPMDJ-2228: RAII wrapper for LoadLibrary-FreeLibrary.

--- Drill4dotNet/Connector/Connector.cpp
+++ Drill4dotNet/Connector/Connector.cpp
@@ -16,14 +16,107 @@
 
 namespace Drill4dotNet
 {
+    class DllLoader
+    {
+    private:
+        class Deleter
+        {
+        private:
+            const std::filesystem::path m_fileName;
+
+        public:
+            Deleter(const std::filesystem::path& fileName)
+                : m_fileName(fileName)
+            {
+            }
+
+            void operator()(const HMODULE library) const noexcept
+            {
+                if (library == nullptr)
+                {
+                    return;
+                }
+
+                if (::FreeLibrary(library) == FALSE)
+                {
+                    std::wcout
+                        << L"Failed to unload library "
+                        << m_fileName.wstring()
+                        << std::endl;
+                }
+            }
+        };
+
+        using UniquePtr = std::unique_ptr<std::remove_pointer_t<HMODULE>, Deleter>;
+
+        static UniquePtr Create(const std::filesystem::path& fileName)
+        {
+            Deleter deleter { fileName };
+
+            const HMODULE nakedPointer { ::LoadLibraryW(fileName.wstring().c_str()) };
+            if (nakedPointer == NULL)
+            {
+                throw std::runtime_error(
+                    std::string { "Cannot load " } + fileName.string());
+            }
+
+            return UniquePtr(nakedPointer, std::move(deleter));
+        }
+
+        UniquePtr m_handle;
+
+    public:
+        DllLoader(const std::filesystem::path& fileName)
+            : m_handle { Create(fileName)}
+        {
+        }
+
+        HMODULE Handle() const noexcept
+        {
+            return m_handle.get();
+        }
+    };
+
+    class AgentConnectorDllLoader
+    {
+    private:
+        inline static const std::filesystem::path CONNECTOR_DLL_FILE { L"agent_connector.dll" };
+
+        const DllLoader m_library { CONNECTOR_DLL_FILE };
+
+        template <typename T>
+        T ImportFunction(LPCSTR const name)
+        {
+            void* result = ::GetProcAddress(m_library.Handle(), name);
+            if (result == nullptr)
+            {
+                throw std::runtime_error(
+                    std::string("Cannot get address of procedure: ")
+                    + name
+                    + " in "
+                    + CONNECTOR_DLL_FILE.string());
+            }
+
+            return reinterpret_cast<T>(result);
+        }
+    public:
+        decltype(::agent_connector_symbols)* const agent_connector_symbols;
+        decltype(::initialize_agent)* const initialize_agent;
+        decltype(::sendMessage)* const sendMessage;
+
+        AgentConnectorDllLoader()
+            : agent_connector_symbols { ImportFunction<decltype(agent_connector_symbols)>("agent_connector_symbols") },
+            initialize_agent { ImportFunction<decltype(initialize_agent)>("initialize_agent") },
+            sendMessage { ImportFunction<decltype(sendMessage)>("sendMessage") }
+        {
+        }
+    };
+
     class Connector : public IConnector
     {
     protected:
-        static const std::filesystem::path CONNECTOR_DLL_FILE;
-        static HMODULE s_library;
-        static decltype(agent_connector_symbols)* fn_agent_connector_symbols;
-        static decltype(initialize_agent)* fn_initialize_agent;
-        static decltype(sendMessage)* fn_sendMessage;
+        inline static const AgentConnectorDllLoader m_agentConnector{};
+
         static std::queue<std::string> m_messages;
         static std::mutex m_mutex;
         static HANDLE m_event;
@@ -49,33 +142,6 @@ namespace Drill4dotNet
             {
                 throw std::runtime_error("Cannot create an event in Connector.");
             }
-
-            if (s_library = ::LoadLibraryW(CONNECTOR_DLL_FILE.wstring().c_str());
-                !s_library)
-            {
-                throw std::runtime_error("Cannot load agent_connector.dll");
-            }
-
-            const struct
-            {
-                void** address;
-                const char* name;
-            } importFunctions[]
-            {
-                { reinterpret_cast<void**>(&fn_agent_connector_symbols), "agent_connector_symbols"},
-                { reinterpret_cast<void**>(&fn_initialize_agent), "initialize_agent"},
-                { reinterpret_cast<void**>(&fn_sendMessage), "sendMessage"}
-            };
-
-            for (auto f : importFunctions)
-            {
-                *(f.address) = ::GetProcAddress(s_library, f.name);
-                if (*(f.address) == nullptr)
-                {
-                    const std::string message{ std::string("Cannot get adresses of procedure: ") + f.name + "in " + CONNECTOR_DLL_FILE.string() };
-                    throw std::runtime_error(message);
-                }
-            }
         }
 
         ~Connector() override
@@ -83,30 +149,28 @@ namespace Drill4dotNet
             ::SetEvent(m_event); // to finish all waits
             ::WaitForSingleObject(m_event, 0);
             ::CloseHandle(m_event);
-
-            if (s_library != nullptr)
-            {
-                if (!::FreeLibrary(s_library))
-                {
-                    std::wcout << L"Failed to unload library " << CONNECTOR_DLL_FILE.wstring() << std::endl;
-                }
-            }
         }
 
         void InitializeAgent() override
         {
             std::wcout << "Connector::InitializeAgent start." << std::endl;
-            agent_connector_ExportedSymbols* ptr = fn_agent_connector_symbols();
+            agent_connector_ExportedSymbols* ptr = m_agentConnector.agent_connector_symbols();
             void (*fun)(const char*, const char*) = ReceiveMessage;
             void* function = (void*)(fun);
-            fn_initialize_agent("mysuperAgent", "localhost:8090", "1.0.0", "group", "fail", function);
+            m_agentConnector.initialize_agent(
+                "mysuperAgent",
+                "localhost:8090",
+                "1.0.0",
+                "group",
+                "fail",
+                function);
             std::wcout << "Connector::InitializeAgent end." << std::endl;
         }
 
         void SendMessage1(const std::string& content) override
         {
             std::wcout << "Connector::SendMessage1: '" << content.c_str() << "'" << std::endl;
-            fn_sendMessage("10", content.c_str());
+            m_agentConnector.sendMessage("10", content.c_str());
         }
 
         std::optional<std::string> GetNextMessage() override
@@ -138,11 +202,6 @@ namespace Drill4dotNet
         }
     };
 
-    const std::filesystem::path Connector::CONNECTOR_DLL_FILE{ L"agent_connector.dll" };
-    HMODULE Connector::s_library = nullptr;
-    decltype(agent_connector_symbols)* Connector::fn_agent_connector_symbols;
-    decltype(initialize_agent)* Connector::fn_initialize_agent;
-    decltype(sendMessage)* Connector::fn_sendMessage;
     std::queue<std::string> Connector::m_messages;
     std::mutex Connector::m_mutex;
     HANDLE Connector::m_event{ nullptr };
